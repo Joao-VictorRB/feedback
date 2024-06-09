cf4485f82941052c59e3df3842de8938eba8f850
Joao-Victor RB <joaovictor.zuca12@gmail.com>
2024-06-09 20:08:04 +0000

    marcio nao tinha ido os arquivos no ultimo git commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,63 @@
+import { CardContainer } from "./Home.styles";
+import { IconContainer } from "./Home.styles";
+import { ButtonContainer } from "./Home.styles";
+import { ResultContainer } from "./Home.styles";
+
+import iconStarImg from "../assets/icon-star.svg"
+import thankYouImg from "../assets/illustration-thank-you.svg"
+import { useState } from "react";
+
+export function Home() {
+const [mostrarResultado, setMostarResultado] = useState(false)
+const [feedbackNote, setfeedbackNote] = useState(0)
+
+function handleFeedbackButtonClick(event){
+ const feedback = Number(event.target.innerText)
+
+ setfeedbackNote(feedback)
+}
+
+function handleSubmit(){
+  if (feedbackNote === 0) return
+
+  setMostarResultado(true)
+}
+
+  return (
+    mostrarResultado === false ?(
+
+    <CardContainer>
+    <IconContainer>
+    <img src={iconStarImg} alt="ícone de estrela" />
+    </IconContainer>
+
+    <h1>Como foi o atendimento?</h1>
+    <p>Conte-nos como foi nossa ajuda com sua solicitação. Agradecemos muito seu feedback para podermos melhorar nosso atendimento!</p>
+
+    <ButtonContainer>
+      <button onClick={handleFeedbackButtonClick}>1</button>
+      <button onClick={handleFeedbackButtonClick}>2</button>
+      <button onClick={handleFeedbackButtonClick}>3</button>
+      <button onClick={handleFeedbackButtonClick}>4</button>
+      <button onClick={handleFeedbackButtonClick}>5</button>
+    </ButtonContainer>
+
+    <button onClick={handleSubmit}>Enviar</button>
+    </CardContainer>
+    ): (
+      <CardContainer>
+        <img src={thankYouImg} alt="" />
+
+      <ResultContainer>
+       <p>Você selecionou {feedbackNote} de 5 </p>
+      </ResultContainer>
+
+      <h1>Obrigado!</h1>
+
+      <p>Agradecemos por dedicar um momento para nos avaliar. Se precisar de mais suporte, não hesite em entrar em contato!</p>
+
+      </CardContainer>
+    )
+      
+  )
+}
